92d0469ae86d6e7e7aafb6c7a33ceaff6fd32e11
Rachel Lynne Wilkerson <rachelw52@gmail.com>
2019-04-24 16:33:23 +0200

    added new ex w asia data

---
+++ asia_ex.R
@@ -0,0 +1,11 @@
+library(bnlearn)
+
+data(asia)
+asia.dag = model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]") #this is the candidate model from pg 240
+graphviz.plot(asia.dag)
+
+asia.bn.fit <- bn.fit(asia.dag, asia)
+
+node.idx = 3;dag = asia.dag;alpha = 2;df = asia
+
+global.monitor.bn(node.idx = 3,dag = asia.dag,alpha = 2,df = asia)
