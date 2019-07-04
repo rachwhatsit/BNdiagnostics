eb1036f15ffdf65e3f09d45103c09c0b133ed9af
Rachel Lynne Wilkerson <rachelw52@gmail.com>
2019-07-04 18:28:04 +0100

    who knows

--- bnmonitoR/R/pach_monitor.R
+++ bnmonitoR/R/pach_monitor.R
@@ -5,54 +5,73 @@
 #'@param pa.names names of parents in the network
 #'@param pa.val names of values to test
 #'@param which.val which level of the value to test
-seq.pa.ch.monitor <- function(dframe, dag, node.name, pa.names, pa.val, node.val){#takes input from bnlearn
-  df <- filter(dframe,!!(sym(node.name))==node.val)
+seq.pa.ch.monitor <- function(df, dag, node.name, pa.names, pa.val){#takes input from bnlearn
+  #df <- filter(dframe,!!(sym(pa.names))==pa.val)
   nodes <-nodes(dag)
-  node.idx <- which(node.name ==colnames(df))
   num.nodes <- length(dag$nodes)
   num.ch <- map(dag$nodes, `[[`, "children")  %>% map_int(length)
   num.values <- map_int(1:num.nodes, function(i){length(unique(dframe[,i]))})
-  
+  node.idx <- which(colnames(df)==node.name)#TODO test that this exists 
   alpha.vec <- rep(alpha/num.values[node.idx], num.values[node.idx])
+  #alpha.vec <- c(0.8,0.2)
   new.alpha <- alpha.vec #this is the one with learning
   counts <- rep(0,num.values[node.idx])
-  sVEC <- rep(0,dim(df)[1]) ;s.learnVEC <- rep(0,dim(df)[1])
-  #e <- rep(0,dim(df)[1]) ;e.learn <- rep(0,dim(df)[1])
-  #v <- rep(0,dim(df)[1]) ;v.learn <- rep(0,dim(df)[1])
+  idx <- which(all(df[,which(colnames(df) %in% pa.names)]==pa.val))
+  
+  #c <- num.values[node.idx]
+  s <- rep(0,dim(df)[1]) ;s.learn <- rep(0,dim(df)[1])
+  e <- rep(0,dim(df)[1]) ;e.learn <- rep(0,dim(df)[1])
+  v <- rep(0,dim(df)[1]) ;v.learn <- rep(0,dim(df)[1])
   z <- rep(0,dim(df)[1]) ;z.learn <- rep(0,dim(df)[1])
-  actual.prop <- rep(0,dim(df)[1]) 
-  theor.prop <- rep(0,dim(df)[1]) ;theor.prop.learn <- rep(0,dim(df)[1])
-  for(i in 1:dim(df)[1]){
+  n <- 0
+  
+  for(i in 1:(dim(df)[1]-1)){
     if(all(df[i,pa.names]==pa.val)){
       counts[as.numeric(df[i,node.idx])] <- counts[as.numeric(df[i,node.idx])] + 1
       new.alpha[as.numeric(df[i,node.idx])] <- new.alpha[as.numeric(df[i,node.idx])] + 1
+      which.val <- as.numeric(df[(i+1),node.idx])
+      p <- (alpha.vec)/sum(alpha.vec)
+      n <- n+1
+      
+      s[i] <- -log(p)[which.val]
+      e[i] <- -sum((p*log(p)))
+
+      p.learn <- (new.alpha) /sum(new.alpha)
+      #p.learn <- lgamma(sum(new.alpha))-sum(lgamma(new.alpha)) + sum(lgamma(new.alpha + counts)) - lgamma(sum(new.alpha + counts))
+      s.learn[i] <- -log(p.learn)[which.val]
+      
+      e.learn[i] <- -sum((p.learn*log(p.learn)))
+      pi <- e[i]/n; pi.learn <- e.learn[i]/n;
+      
+      v[i] <- (sum(p*log(p)^2)) -e[i]^2#+(pi^2/12)
+      #v[i] <- n*pi*(1-pi)
+      z[i] <- (cumsum(s)[i]-cumsum(e)[i])/sqrt(cumsum(v)[i])
+      v.learn[i] <- (sum(p.learn*log(p.learn)^2)) -e.learn[i]^2#-(c^2/12)
+      z.learn[i] <- (cumsum(s.learn)[i]-cumsum(e.learn)[i])/sqrt(cumsum(v.learn)[i])
     } else {
       new.alpha[as.numeric(df[i,node.idx])] <- new.alpha[as.numeric(df[i,node.idx])] + 1
+      next
     }
-    which.val <- as.numeric(df[i,node.idx])
-    p <- (alpha.vec +counts)/sum(alpha.vec+counts)
-    actual.prop[i] <-counts[which.val]/sum(counts)
-    theor.prop[i] <- p[which.val]/sum(p)
-    s <- -log(p)[which.val]
-    sVEC[i] <- -log(p)[which.val]
-    e <- sum(-(p*log(p)))
-    v <- sum(p*log(p)^2) -e^2
-    #z[i] <- (sum(s)-sum(e))/sqrt(sum(v))
-    z[i] <- (s-e)/sqrt(v)
-    p.learn <- (new.alpha) /sum(new.alpha)
-    theor.prop.learn[i] <- p.learn[which.val]/sum(p.learn)
-    s.learn <- -log(p)[which.val]
-    s.learnVEC[i] <- -log(p)[which.val]
-    e.learn <- sum(-(p.learn*log(p.learn)))
-    v.learn <- sum(p.learn*log(p.learn)^2) -e.learn^2
-    #z.learn <- (sum(s.learn)-sum(e.learn))/sqrt(sum(v.learn))
-    z.learn[i] <- ((s.learn)-(e.learn))/sqrt((v.learn))
+    
+
   }
-  t <- 1:dim(df)[1]
-  pa.title <- toString(paste0(pa.names," = ",pa.val))
-  as.data.frame(cbind(t,z,z.learn)) %>% 
-    reshape2::melt(id='t') %>% #TODO add the variable ggtitle to this plot 
-    ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic') + theme_minimal() + 
-    ggtitle(paste0("p(",node.name," = ",node.val, " | ", pa.title, ")")) + 
-    scale_colour_discrete(name="", labels=c("Expert's prior, without learning", "Expert's prior with learning")) + theme(legend.position="bottom")
+  #par(mfrow=c(1,2))
+  #plot(s.learn)
+  z.learn[which(z.learn==0)] <- NA
+  plot(z.learn)
+  return(z.learn)
+  #result <- z.learn[which(z.learn!=0)]
+  #return(result)
+
+
 }
+
+
+pa.ch.graph <-  function(z.learn)
+  t <- 1:length(z.learn[which(z.learn!=0)])
+pa.title <- toString(paste0(pa.names," = ",pa.val))
+as.data.frame(cbind(t,z.learn[which(z.learn!=0)])) %>%
+  reshape2::melt(id='t') %>%
+  ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic') + theme_minimal() +
+  ggtitle(paste0("p(",node.name," | ", pa.names, " =", pa.val, " ) ")) + #TODO fix the label here
+  scale_colour_discrete(name="", labels=c( "Reference prior with learning")) + theme(legend.position="bottom")
--- chds_example.R
+++ chds_example.R
@@ -124,46 +124,43 @@ pa.ch.bn.monitor <- function(node.idx, num.pa.combo, pa.combo.idx, df, alpha){
 node.idx=3; num.pa.combo=chds.gs.num.pa.combo; pa.combo.idx = 3; df = df;alpha = 3
 pa.ch.bn.monitor(node.idx=3, num.pa.combo=chds.gs.num.pa.combo, pa.combo.idx = 3, df = df,alpha = 3)->test
 
-seq.pa.ch.monitor <- function(df, node.idx, pa.names, pa.val,which.val){
-  alpha.vec <- rep(alpha/num.values[node.idx], num.values[node.idx])
-  new.alpha <- alpha.vec
-  counts <- rep(0,num.values[node.idx])
-  z <- rep(0,dim(df)[1])
-  z.learn <- rep(0,dim(df)[1])
-  for(i in 1:dim(df)[1]){
-    if(df[i,][[pa.names]]==pa.val){#TODO change this to accommodate parent pairs (use any)
-      counts[as.numeric(df[i,node.idx])] <- counts[as.numeric(df[i,node.idx])] + 1
-      new.alpha[as.numeric(df[i,node.idx])] <- new.alpha[as.numeric(df[i,node.idx])] + 1
-    } else {
-      counts <- counts
-      new.alpha[as.numeric(df[i,node.idx])] <- new.alpha[as.numeric(df[i,node.idx])] + 1
-    }
-    p <- (alpha.vec + counts)/sum(alpha.vec + counts)
-    s <- -log(p)
-    e <- -sum(p*(log(p)))
-    v <- sum(p*(log(p))^2)
-    z[i] <- (s[which.val]-e)/sqrt(v)
-    p.learn <- (new.alpha + counts)/sum(new.alpha + counts)
-    s.learn <- -log(p.learn)
-    e.learn <- -sum(p.learn*(log(p.learn)))
-    v.learn <- sum(p.learn*(log(p.learn))^2)
-    z.learn[i] <- (s.learn[which.val]-e.learn)/sqrt(v.learn)
-  }
-  t <- 1:dim(df)[1]
-  as.data.frame(cbind(t,z,z.learn)) %>% 
-    reshape2::melt(id='t') %>% #TODO add the variable ggtitle to this plot 
-    ggplot(aes(x=t, y=value, colour=variable)) + geom_line() + xlab('Relevant sample size') + ylab('Standardized Z Statistic') + theme_minimal() + 
-      scale_colour_discrete(name="", labels=c("Expert's prior, without learning", "Expert's prior with learning")) + theme(legend.position="bottom")
-}
 
-df=df; node.idx = 3;pa.names = "Social";pa.val = "High"; which.val=1
+df=df; node.name ="Admission" ;pa.names = c("Social", "EVents");pa.val = c("Low","High"); which.val=1
 
-seq.pa.ch.monitor(df=df, node.idx = 3,pa.names = "Social",pa.val = "High", which.val = 1) 
+bn.pach <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Admission", pa.names = c("Social","Events"),pa.val = c("Low","High")) 
+bn.pach.x <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Economic", pa.names = "Social",pa.val = "High") 
+bn.pach2 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Economic", pa.names = "Social",pa.val = "Low") 
 
+bn.pache1 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Events", pa.names = c("Social", "Economic"),pa.val = c("Low", "Low")) 
+bn.pache2 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Events", pa.names = c("Social", "Economic"),pa.val = c("Low", "High")) 
+bn.pache3 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Events", pa.names = c("Social", "Economic"),pa.val = c("High", "Low")) 
+bn.pache4 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Events", pa.names = c("Social", "Economic"),pa.val = c("High", "High")) 
 #node diagnostics  
+t <- 1:length(bn.pache1)
+as.data.frame(cbind(t,bn.pache1,bn.pache2,bn.pache3,bn.pache4)) %>%
+  reshape2::melt(id='t') %>%
+  ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic')  +theme_minimal(base_size = 22) +
+  ggtitle("Parent-Child Monitor for Life Events") + #TODO fix the label here1:length(chds.ahc.pach31)
+ggsave("C:/Users/rachel/Documents/diagpaper/ba/img/chdsBNevents.jpeg")
+
+bn.pacheh1 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Admission", pa.names = c("Social", "Events"),pa.val = c("Low", "Low")) 
+bn.pacheh2 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Admission", pa.names = c("Social", "Events"),pa.val = c("Low", "High")) 
+bn.pacheh3 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Admission", pa.names = c("Social", "Events"),pa.val = c("High", "Low")) 
+bn.pacheh4 <- seq.pa.ch.monitor(df = df, dag = chds.dag, node.name  = "Admission", pa.names = c("Social", "Events"),pa.val = c("High", "High")) 
+#node diagnostics  
+t <- 1:length(bn.pache1)
+as.data.frame(cbind(t,bn.pacheh1,bn.pacheh2,bn.pacheh3,bn.pacheh4)) %>%
+  reshape2::melt(id='t') %>%
+  ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic')  +theme_minimal(base_size = 22) +
+  ggtitle("Parent-Child Monitor for Admission") + #TODO fix the label here
+  scale_colour_discrete(name="", labels=c( "Xs=Low, Xe=Low", "Xs=Low, Xe=High","Xs=High, Xe=Low","Xs=High, Xe=High")) + theme(legend.position="right")
+ggsave("C:/Users/rachel/Documents/diagpaper/ba/img/chdsBNpachH.jpeg")
 
 
-df <- read.csv(file = "CHDS.latentexample1.csv")
+
+
+
+
 
 hl <- c('High', 'Low')
 ahl <- c('Average', 'High', 'Low')
@@ -173,10 +170,11 @@ ny <- c("No", "Yes")
 df %>% filter(Social == 'Low', Events=='Low') %>% count(Admission)
 
 #how to functionize these levels t
-s <- cptable(~Social, values=c(507,383),levels=hl)
-e.s <-cptable(~Economic|Social, values = c(237, 270,46,337), levels=hl)
-l.se <- cptable(~Events|Social+Economic, values=c(86,32,119,900,65,115,14,11,21,105,158,74), levels=ahl)
-h.sl <- cptable(~Admission|Social+Events, values=c(146,30,73,24,213,21,87,32,125,44,77,18),levels=ny)
+#THESE ARE THE M-1 COUNTS FOR THE DATA, LEAVING OUT THE LAST OBSERATIO 890 HIGH HIGH YES 
+s <- cptable(~Social, values=c(506,383),levels=hl)
+e.s <-cptable(~Economic|Social, values = c(236, 270,45,337), levels=hl)
+l.se <- cptable(~Events|Social+Economic, values=c(86,31,119,900,64,115,14,10,21,105,157,74), levels=ahl)
+h.sl <- cptable(~Admission|Social+Events, values=c(146,29,73,23,213,20,87,31,125,43,77,17),levels=ny)
 
 plist <- compileCPT(list(s, e.s, l.se, h.sl))
 plist#check the network out
@@ -195,7 +193,7 @@ abs_stand <- function(i,j){ #returns z score for the ith node with the jth worst
 }
 
 map2(1:4, c(2,2,3,2), abs_stand) #marginal node monitors for the CHDS 
-map2(1:4, c(1,1,1,1), abs_stand) #marginal node monitors for the CHDS 
+map2(1:4, c(1,1,2,2), abs_stand) #marginal node monitors for the CHDS 
 
 
 net1t <- setEvidence(net1,evidence=list(Social='High',Economic='High',Events='High',Admission='Yes'))
@@ -212,7 +210,7 @@ net1h <- setEvidence(net1,evidence=list(Social='High',Economic='High',Events='Hi
 h <- querygrain(net1h, nodes=c('Admission'))
 
 cond_abs_stand <- function(marg,j){ #returns z score for the ith node with the jth worst level
-  p <- marg[[1]][j] #pick the worst one
+  p <- marg[j] #pick the worst one
   sm <- -log(p)
   em <- - sum(marg[[1]]*log(marg[[1]]))
   vm <- sum(marg[[1]]*(log(marg[[1]])^2)) - em^2
@@ -221,16 +219,18 @@ cond_abs_stand <- function(marg,j){ #returns z score for the ith node with the j
 }
 
 #conditional node monitors 
-cond_abs_stand(s,2)
-cond_abs_stand(e,2)
-cond_abs_stand(l,3)
+cond_abs_stand(s,1)
+cond_abs_stand(e,1)
+cond_abs_stand(l,2)
 cond_abs_stand(h,2)
 
 
-cond_abs_stand(c(0.75,0.25),1)
+#taking the CPTs from the tree 
 cond_abs_stand(c(0.57,0.43),1)
-cond_abs_stand(c(0.14,0.36,0.5),1)
-cond_abs_stand(c(0.14,0.36,0.5),2)
+cond_abs_stand(c(0.47,0.53),1)
+cond_abs_stand(c(0.2,0.34,0.46),2)
+cond_abs_stand(c(0.74,0.26),2)
+
 cond_abs_stand(c(0.14,0.36,0.5),3)
 
 #how can we functionize this?
@@ -245,24 +245,26 @@ standardize <- function(vec,j){ #returns z score for the ith node with the jth w
   return(zm)
 }
 
+marg_monitor(c(.57,.43),1)
 marg_monitor(c(.57,.43),2)
-marg_monitor(c(.57,.43),2)
-.57*.47+.43*.12
-.57*.53+.43*.88
+xe <-c(.57*.47+.43*.12,
+.57*.53+.43*.88)
+marg_monitor(xe,1)
 marg_monitor(c(.3195,.6805),1)
 marg_monitor(c(.3195,.6805),2)
 
-xl <- c(.57*.47*.50 + .57*.53*.43 + .43*.12*.45 + .43*.88*.22,
-        .57*.47*.36 + .57*.53*.33 + .43*.12*.31 + .43*.88*.31,
-        .57*.47*.14 + .57*.53*.24 + .43*.12*.24 + .43*.88*.47)#p(Xl = Low )
+xl <- c(.57*.47*.50 + .57*.53*.43 + .43*.12*.45 + .43*.88*.22,#LOW
+        .57*.47*.36 + .57*.53*.33 + .43*.12*.31 + .43*.88*.31,#MEDIUM
+        .57*.47*.14 + .57*.53*.24 + .43*.12*.24 + .43*.88*.47)#HIGH
 
-marg_monitor(xl,1)
+marg_monitor(xl,3)
 
-.57*.47*.14*.25 + .57*.53*.24*.25 +.57*.47*.36*.17 + .57*.53*.33*.17+
-  .57*.47*.50*.09 + .57*.53*.33*.09+
-  .43*.47*.24*.26 + .43*.53*.47*.26+
-  ,43*.47*.31*.27 + .43*.53*.31*.27+
-  .43*.47*.45*.19 + .43*.53*.22*.19 -> pyes
+  .57*.47*.50*.25 + .57*.53*.43*.25 +
+  .57*.47*.36*.17 + .57*.53*.33*.17+
+  .57*.47*.14*.09 + .57*.53*.24*.09+
+  .43*.47*.45*.26 + .43*.53*.22*.26+
+  .43*.47*.31*.27 + .43*.53*.31*.27+
+  .43*.47*.24*.19 + .43*.53*.47*.19 -> pyes
 
 xh <-c(1-pyes,pyes)
 marg_monitor(xh,2)
@@ -340,5 +342,35 @@ pass.ev <-function(i){
   return(evi)
 }
 
+z.s <- marg.node.monitor(chds.dag,df,"Social")
+z.e <- marg.node.monitor(chds.dag,df,"Economic")
+z.l <- marg.node.monitor(chds.dag,df,"Events")
+z.h <- marg.node.monitor(chds.dag,df,"Admission")
 
+
+plot(z.e)
+plot(z.l)
+plot(z.h)
+
+z.s.cond <- cond.node.monitor(chds.dag,df,"Social")
+head(z.s.cond)
+z.e.cond <- cond.node.monitor(chds.dag,df,"Economic")
+z.l.cond <- cond.node.monitor(chds.dag,df,"Events")
+z.h.cond <- cond.node.monitor(chds.dag,df,"Admission")
+
+t <- 1:length(z.l.cond[,1])
+as.data.frame(cbind(t,z.l.cond[,1],z.l.cond[,2])) %>%
+  reshape2::melt(id='t') %>%
+  ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic')  +theme_minimal(base_size = 22) +
+  ggtitle("Node Monitors for Life Events") + #TODO fix the label here
+  scale_colour_discrete(name="", labels=c( "Marginal", "Conditional")) + theme(legend.position="right")
+ggsave("C:/Users/rachel/Documents/diagpaper/ba/img/nodeBNevents.jpeg")
+
+t <- 1:length(z.l.cond[,1])
+as.data.frame(cbind(t,z.h.cond[,1],z.h.cond[,2])) %>%
+  reshape2::melt(id='t') %>%
+  ggplot(aes(x=t, y=value, colour=variable)) + geom_point() + xlab('Relevant sample size') + ylab('Standardized Z Statistic')  +theme_minimal(base_size = 22) +
+  ggtitle("Node Monitors for Admission") + #TODO fix the label here
+  scale_colour_discrete(name="", labels=c( "Marginal", "Conditional")) + theme(legend.position="right")
+ggsave("C:/Users/rachel/Documents/diagpaper/ba/img/nodeBNh.jpeg")
 
